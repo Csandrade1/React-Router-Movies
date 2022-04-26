c5a9cd6237abcd23808d1bce3768ad036e026089
Csandrade1 <andrade.cynthia07@gmail.com>
2022-04-26 00:57:14 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -39,10 +40,12 @@ export default function App() {
         }
       />
 
-      <Route path="/">
+      <Route exact path="/">
         <MovieList movies={movieList} />
       </Route>
-      <Route path={"/movies/:movieId"}></Route>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
     </div>
   );
 }
